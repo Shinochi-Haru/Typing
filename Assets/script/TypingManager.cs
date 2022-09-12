cf2f1625d2855a892cb448fef6d7b32cede0151e
Shinochi-Haru <haru060109011305@gmail.com>
2022-09-12 18:07:00 +0900

    [add]ツールの追加・TypingManagerの生成

---
+++ Assets/script/TypingManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text.Json;
+using Csv;
+using TMPro;
+using System.Text;
+using System.IO;
+
+public class TypingManager : MonoBehaviour
+{
+    private TextMeshPro sentence;
+    private TextMeshPro hiragana;
+    private TextMeshPro alp;
+    private TextMeshPro typing_count;
+    private TextMeshPro miss_typ_count;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
